146be669ebf0bfd7156dffdfa5efe42620a78dbb
TangibleNetworks <tangible.networks@gmail.com>
2014-05-16 09:25:27 +0100

    initial commit

---
+++ TN.cpp
@@ -0,0 +1,316 @@
+/* 
+  TN.cpp - Library for TN-04
+  Tangible Networks
+  Espen Knoop, 15th May 2014
+*/
+
+
+#include "Arduino.h"
+#include "TN.h"
+
+
+// Constructor
+TN::TN(float minVal, float maxVal) {
+  _minVal = minVal;
+  _maxVal = maxVal;
+  pinMode(IN1_A,INPUT);
+  pinMode(IN1_HS,INPUT);
+  pinMode(IN2_A,INPUT);
+  pinMode(IN2_HS,INPUT);
+  pinMode(IN3_A,INPUT);
+  pinMode(IN3_HS,INPUT);
+  pinMode(LED_R,OUTPUT);
+  pinMode(LED_G,OUTPUT);
+  pinMode(LED_B,OUTPUT);
+  pinMode(SW,INPUT);
+  ::digitalWrite(SW,HIGH);
+  _dacSetup();
+  // Go through all the functions, to initialise state vars
+  for (int i=1; i<4; i++) {
+    analogWrite(i,_minVal);
+    analogRead(i);
+  }
+  colour(0,0,0);
+  dip1();
+  dip2();
+  dip3();
+  masterRead();
+  pot();
+  sw();
+}
+
+
+// For DAC
+void TN::_dacSetup() {
+  // Set pins to output
+  sbi(I2C_DDR,I2C_CLK);
+  sbi(I2C_DDR,I2C_DAT);
+  sbi(I2C_DDR,I2C_CS1);
+  sbi(I2C_DDR,I2C_CS2);
+}
+
+inline void TN::_startcond() {
+  I2C_DATA_LO();
+  delayMicroseconds(5);
+  I2C_CLOCK_LO();
+  ;
+}
+
+inline void TN::_stopcond() {
+  I2C_CLOCK_HI();
+  delayMicroseconds(5);
+  I2C_DATA_HI();
+  delayMicroseconds(5);
+}
+
+// Write value to output (will be clipped to range [minVal, maxVal]
+void TN::analogWrite(int output, float floatVal) {
+  floatVal = MINMAX(floatVal,_minVal,_maxVal);
+  int value = floor(4095.0* (floatVal - _minVal)/(_maxVal - _minVal));
+  int address = 0;
+  switch (output) {
+    case 3:
+      address = 0;
+      I2C_CS1_HI();
+      I2C_CS2_HI();
+      _outs[2] = floatVal;
+      break;
+    case 2:
+      address = 1;
+      I2C_CS1_LO();
+      I2C_CS2_HI();
+      _outs[1] = floatVal;
+      break;
+    default: // pick 1 as default
+      address = 1;
+      I2C_CS1_HI();
+      I2C_CS2_LO();
+      _outs[0] = floatVal;
+  }
+  _startcond();
+  I2C_WRITEBIT(1);
+  I2C_WRITEBIT(1);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(address);
+  I2C_WRITEBIT(0);
+  
+  I2C_WRITEBIT(1);
+  
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+  I2C_WRITEBIT(0);
+
+  for (int i=11; i > 7; i--) {
+    I2C_WRITEBIT(1 & (value >> i) );
+  }
+  
+  I2C_WRITEBIT(1);
+  
+  for (int i=7; i >= 0; i--) {
+    I2C_WRITEBIT(1 & (value >> i) );
+  }
+  
+  I2C_WRITEBIT(1);
+  _stopcond();
+}
+
+
+// Set LED to RGB colour (ints, [0, 255])
+void TN::colour(int r, int g, int b) {
+  r = MINMAX(r,0,255);
+  g = MINMAX(g,0,255);
+  b = MINMAX(b,0,255);
+  _colour[0] = r;
+  _colour[1] = g;
+  _colour[2] = b;
+  ::analogWrite(LED_R,r);
+  ::analogWrite(LED_G,g);
+  ::analogWrite(LED_B,b);
+}
+
+
+// Set LED to RGB colour (floats, [0.0, 1.0])
+void TN::colour(float r, float g, float b) {
+  colour((int)(r*255.0),(int)(g*255.0),(int)(b*255.0));
+}
+ 
+
+// Check if an input is connected
+boolean TN::isConnected(int input) {
+  switch (input) {
+    case 3:
+      return ::digitalRead(IN3_HS);
+      break;
+    case 2:
+      return ::digitalRead(IN2_HS);
+      break;
+    default:
+      return ::digitalRead(IN1_HS);  
+  }  
+}
+
+
+// Read the analog value from input (returns float in [minVal, maxVal])
+// Returns minVal if input is not connected
+float TN::analogRead(int input) {
+  int idx;
+  if (isConnected(input)) {
+    int rawVal;
+    switch (input) {
+      case 3:
+        rawVal = ::analogRead(IN3_A);
+        idx = 2;
+        break;
+      case 2:
+        rawVal = ::analogRead(IN2_A);
+        idx = 1;
+        break;
+      default:
+        rawVal = ::analogRead(IN1_A);
+        idx = 0;
+    }
+    _ins[idx] = _minVal + (_maxVal - _minVal)*rawVal/1023.0;
+    
+  }
+  else _ins[idx] = _minVal;
+  return _ins[idx];
+}
+
+
+// Read the digital value (0 or 1) from input (returns 0 if not connected)
+int TN::digitalRead(int input) {
+  switch (input) {
+    case 3:
+      if (isConnected(3)) _ins[2] = ::digitalRead(IN3_A);
+      else _ins[2] = 0;
+      return ::digitalRead(IN3_A);
+    case 2:
+      if (isConnected(2)) _ins[1] = ::digitalRead(IN2_A);
+      else _ins[1] = 0;
+      return _ins[1];
+    default: // assume 1
+      if (isConnected(1)) _ins[0] = ::digitalRead(IN1_A);
+      else _ins[0] = 0;
+      return _ins[0];
+  }
+}
+
+
+// Write digital value (0 or 1) to output
+void TN::digitalWrite(int output, int value) {
+  if (value) analogWrite(output, _maxVal);
+  else analogWrite(output, _minVal);
+}
+
+
+// Get state of DIP switches (1 for on)
+boolean TN::dip1() {
+  if (0b100 & _dip()) _dips[0] = 1;
+  else _dips[0] = 0;
+  return _dips[0];
+}
+
+boolean TN::dip2() {
+  if (0b010 & _dip()) _dips[1] = 1;
+  else _dips[1] = 0;
+  return _dips[1];
+}
+
+boolean TN::dip3() {
+  if (0b001 & _dip()) _dips[2] = 1;
+  else _dips[2] = 0;
+  return _dips[2];
+}
+
+
+// Get state of switch (1 is pressed)
+boolean TN::sw() {
+  _sw = ! ::digitalRead(SW);
+  return (_sw);
+}
+
+
+// Get position of pot (float, [0.0, 1.0])
+float TN::pot() {
+  _pot = ::analogRead(POT)/1023.0;
+  return _pot;
+}
+
+
+// 1 if master controller is connected
+boolean TN::masterConnected() {
+  return (!::digitalRead(MASTER_HS));
+}
+
+
+// Value of master controller (float, [0.0, 1.0])
+float TN::masterRead() {
+  if (masterConnected()) {
+    _master = ::analogRead(MASTER_A)/1023.0;
+  }
+  else _master = 0.0;
+  return _master;
+}
+
+
+// Process dip input
+int TN::_dip() {
+
+  int val = ::analogRead(DIP);
+
+  // 000 1023
+  if (val > 895) return 0b000;
+  // 001 768
+  else if (val > 725) return 0b001;
+  // 010 682
+  else if (val > 646) return 0b010;
+  // 100 610
+  else if (val > 584) return 0b100;
+  // 011 557
+  else if (val > 533) return 0b011;
+  // 101 509
+  else if (val > 489) return 0b101;
+  // 110 469
+  else if (val > 438) return 0b110;
+  // 111 407
+  else return 0b111;
+}
+
+// Print current state to serial 
+// (takes ~5 ms to execute.  Requires Serial.begin(115200) in setup(). )
+void TN::printState() {
+  Serial.print("RGB: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(_colour[i]);
+    Serial.print(", ");
+  }
+  Serial.print("Ins: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(_ins[i]);
+    Serial.print(", ");
+  }
+  Serial.print("Outs: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(_outs[i]);
+    Serial.print(", ");
+  }
+  Serial.print("Pot: ");
+  Serial.print(_pot);
+  Serial.print(", Master: ");
+  Serial.print(_master);
+  Serial.print(", DIPs: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(_dips[i]);
+    Serial.print(", ");
+  }
+  Serial.print("Sw: ");
+  Serial.println(_sw);
+}
+
+
+
+    
---
+++ TN.h
@@ -0,0 +1,140 @@
+/* 
+  TN.h - Library for TN-04
+  Tangible Networks
+  Espen Knoop, 15th May 2014
+*/
+
+#ifndef TN_h
+#define TN_h
+
+#include "Arduino.h"
+
+// Clear bit, set bit
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif 
+
+
+// Fast min, max
+#define MIN(x,y) x < y ? x : y
+#define MAX(x,y) x > y ? x : y
+#define MINMAX(x,lower,upper) MIN(MAX(x,lower),upper)
+
+
+// Pins
+#define LED_R 11
+#define LED_G 6
+#define LED_B 5
+#define POT A7
+#define SW 2
+#define DIP A4
+#define AUX_IN A3
+#define AUX_OUT 3
+#define IN1_A A0
+#define IN1_HS 7
+#define IN2_A A1
+#define IN2_HS 4
+#define IN3_A A2
+#define IN3_HS A5
+#define MASTER_A A6
+#define MASTER_HS 8
+
+
+// For DAC
+#define I2C_DDR DDRB
+#define I2C_PIN PINB
+#define I2C_PORT PORTB
+#define I2C_CLK 1
+#define I2C_DAT 2
+#define I2C_CS2 4
+#define I2C_CS1 5
+#define I2C_CLOCK_LO()    cbi(I2C_PORT,I2C_CLK)
+#define I2C_CLOCK_HI()    sbi(I2C_PORT,I2C_CLK) 
+#define I2C_CLOCK_PULSE() {delayMicroseconds(5); I2C_CLOCK_HI(); delayMicroseconds(5); I2C_CLOCK_LO();}
+#define I2C_DATA_LO()     cbi(I2C_PORT,I2C_DAT)
+#define I2C_DATA_HI()     sbi(I2C_PORT,I2C_DAT)
+#define I2C_DATA_SET(v)   if(v){I2C_DATA_HI();}else{I2C_DATA_LO();}
+#define I2C_CS1_LO()      cbi(I2C_PORT,I2C_CS1)
+#define I2C_CS1_HI()      sbi(I2C_PORT,I2C_CS1)
+#define I2C_CS2_LO()      cbi(I2C_PORT,I2C_CS2)
+#define I2C_CS2_HI()      sbi(I2C_PORT,I2C_CS2)
+#define I2C_WRITEBIT(v)   {I2C_DATA_SET(v); I2C_CLOCK_PULSE();}
+
+
+
+class TN {
+  public:
+    // Constructor
+    TN(float minVal=0.0, float maxVal=1.0);
+    
+    // Set LED to RGB colour (ints, [0, 255])
+    void colour(int r, int g, int b);
+    
+    // Set LED to RGB colour (floats, [0.0, 1.0])
+    void colour(float r, float g, float b);
+    
+    // Check if an input is connected
+    boolean isConnected(int input);
+    
+    // Read the analog value from input (returns float in [minVal, maxVal])
+    // Returns minVal if input is not connected
+    float analogRead(int input);
+    
+    // Write value to output (will be clipped to range [minVal, maxVal]
+    void analogWrite(int output, float value);
+    
+    // Read the digital value (0 or 1) from input (returns 0 if not connected)
+    int digitalRead(int input);
+
+    // Write digital value (0 or 1) to output
+    void digitalWrite(int output, int value);
+    
+    // Get state of DIP switches (1 for on)
+    boolean dip1();
+    boolean dip2();
+    boolean dip3();
+    
+    // Get state of switch (1 is pressed)
+    boolean sw();
+    
+    // Get position of pot (float, [0.0, 1.0])
+    float pot();
+    
+    // 1 if master controller is connected
+    boolean masterConnected();
+    
+    // Value of master controller (float, [0.0, 1.0])
+    float masterRead();  
+
+    // Print current state to serial
+    void printState();
+  private:
+    // Process dip input
+    int _dip();
+    
+    // For dac
+    void _dacSetup();
+    void _startcond();
+    void _stopcond();
+  
+    // Defines range of inputs/outputs (set in constructor)
+    float _minVal;
+    float _maxVal;
+
+    // Stores current state (for printing)
+    int _colour[3];
+    float _outs[3];
+    float _ins[3];
+    float _pot;
+    float _master;
+    boolean _dips[3];
+    boolean _sw;
+};
+
+
+#endif
+
+
